1027b8604d71bb68c3ee55b3d26762fc5c473ae3
DanBrandelli <danilobrandelli@gmail.com>
2025-04-29 14:12:22 -0300

    Primeira versão do projeto

---
+++ Fibonacci-MEU.cpp
@@ -0,0 +1,24 @@
+//7. Faça um programa que monte os oito primeiros termos da sequência de Fibonacci
+// 0  1  1  2  3  5  8  13  21  34  55
+#include<stdio.h>
+
+int main ()
+{
+    int p1, p2, resultado, i;
+    p1 = 0;
+    p2 = 1;
+
+     printf ("\t%d", p1);
+     printf ("\t%d", p2);
+
+    for (i = 3; i <=8; i++)
+    {
+         resultado = p1 + p2;
+         printf ("\t%d", resultado); //printei 1 3ª posição
+         p1 = p2;
+         p2 = resultado;
+    }
+
+    getchar ();
+    return 0;
+}
